6ed2e7d661286aafe7975ba592dfe0fcdacaf480
Noha311 <168765235+Noha311@users.noreply.github.com>
2024-07-30 23:34:26 +0300

    Update task3Nti.py

--- task3Nti.py
+++ task3Nti.py
@@ -12,6 +12,43 @@ cast_heights = [72, 68, 72, 66, 76]
 dict(zip(cast_names,cast_heights))
 
 
+# main.py
+import calc  # type: ignore
+
+def get_numbers():
+    while True:
+        try:
+            print("="*30 + " HELLO TO MY 'Mini Calculator' " + "="*24)
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+            return num1, num2
+        except ValueError:
+            print("Invalid input. Please enter numeric values.")
+
+while True:
+        command = input("Enter command (add, sub, mult, div) or 'st' to stop: ").strip().lower()
+        
+        if command == 'st':
+            print("Exiting the program.")
+            break
+        
+        if command in ['add', 'sub', 'mult', 'div']:
+            num1, num2 = get_numbers()
+            
+            if command == 'add':
+                result = calc.add(num1, num2)
+            elif command == 'sub':
+                result = calc.subtract(num1, num2)
+            elif command == 'mult':
+                result = calc.multiply(num1, num2)
+            elif command == 'div':
+                result = calc.divide(num1, num2)
+            
+            print(f"Result: {result}")
+        else:
+            print("Invalid command. Please enter 'add', 'sub', 'mult', 'div' or 'st' to stop.")
+
+
 
 
         
